308bb0508f2be4123b280e4df0523aa7d47ae167
Dalton Whyte <dalton.whyte@extended-content.com>
2018-09-29 20:49:47 +0100

    Updated safer.js
    Added extra guard against shutdownServer and dropDatabase commands.

--- safer.js
+++ safer.js
@@ -1,5 +1,10 @@
-DB.prototype.dropDatabase = function() {
-	print("Cannot drop database!");
-}
+// .mongorc.js script that safe guards
+// against bad dropping or shutting down
+// the mongodb servers
+ _no_op_ = function() { print("Cannot perform action!"); }
 
-db.dropDatabase = DB.prototype.dropDatabase;
+DB.prototype.dropDatabase = _no_op_;
+db.dropDatabase = db.prototype.dropDatabase;
+
+DB.prototype.shutdownServer = _no_op_;
+db.shutdownServer = db.prototype.shutdownServer
